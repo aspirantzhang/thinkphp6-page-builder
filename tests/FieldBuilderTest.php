8f726da0c4ad52ae9b64148ad5adaa8b98f88e53
aspirantzhang <admin@aspirantzhang.com>
2021-03-29 19:51:42 +0800

    test: add FieldBuilderTest

---
+++ tests/FieldBuilderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace aspirantzhang\test\antdBuilder;
+
+use aspirantzhang\TPAntdBuilder\Builder;
+
+class FieldBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEmptyParamsShouldReturnInitSchema()
+    {
+        $actual = Builder::field('');
+        $expected = [
+            'name' => '',
+            'title' => '',
+            'type' => 'text'
+        ];
+        $this->assertEqualsCanonicalizing($expected, (array)$actual);
+    }
+
+    public function testValidParamsShouldReturnCorrectResult()
+    {
+        $actual = (array)Builder::field('unitTest', 'Unit Test')
+                ->type('switch')
+                ->sorter(true)
+                ->editDisabled(true)
+                ->hideInColumn(true)
+                ->mode('multiple')
+                ->data(['unitTestData']);
+        $expected = [
+            'name' => 'unitTest',
+            'title' => 'Unit Test',
+            'type' => 'switch',
+            'data' => ['unitTestData'],
+            'sorter' => true,
+            'editDisabled' => true,
+            'hideInColumn' => true,
+            'mode' => 'multiple',
+        ];
+        $this->assertEqualsCanonicalizing($expected, $actual);
+    }
+}
